273ef2144f622004747af94ce04e0904ff2d9cef
Felipe Azeredo <felipe.azeredo89@gmail.com>
2024-11-19 20:07:41 -0300

    att

--- src/Contador.js
+++ src/Contador.js
@@ -1,5 +1,3 @@
-//Contador.js
-
 import React, { useState } from 'react';
 import './Contador.css';
 
@@ -14,11 +12,13 @@ const Contador = () => {
       <h1>Contador de Homens e Mulheres</h1>
       <div className="contador">
         <div className="pessoa">
+          <img src="/imagens/homem.jpg" alt="Avatar Homem" className="avatar" />
           <h2>Homens: {homens}</h2>
           <button onClick={() => setHomens(homens + 1)}>+</button>
           <button onClick={() => setHomens(homens > 0 ? homens - 1 : 0)}>-</button>
         </div>
         <div className="pessoa">
+          <img src="/imagens/mulher.jpg" alt="Avatar Mulher" className="avatar" />
           <h2>Mulheres: {mulheres}</h2>
           <button onClick={() => setMulheres(mulheres + 1)}>+</button>
           <button onClick={() => setMulheres(mulheres > 0 ? mulheres - 1 : 0)}>-</button>
